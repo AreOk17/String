6689390ad2fd8b2a7a60d85e8f12ccc6fdb4c573
1 <Anasko162@Mail.ru>
2022-11-29 05:25:10 +0300

    Задачи по теме строки часть 1.

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Иван";
+        String middleName = "Иванович";
+        String lastName = "Иванов";
+        String separator = " ";
+        String fullName = String.join(separator, lastName, firstName, middleName);
+        System.out.println("ФИО сотрудника — " +fullName+ ".");
+        String fullName1 = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — "+fullName1.toUpperCase());
+        fullName1 = "Иванов Семён Семёнович".replace("ё","е");
+        System.out.println(fullName1);
+
+    }
+}
